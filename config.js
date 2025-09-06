cebab88d7212525d7886af0648017e6addfeab78
Gaeul <143046394+Gaeuly@users.noreply.github.com>
2025-09-07 05:50:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
-    Token: "NTAwMjkzMzY1NDk0MDU0OTMy.G870MM.c4OTulizWX2PBQ9WYEnnrKPkD0FjE63lwSBhzY",
+    Token: "MTM0MzE2ODk2ODIzNTU1MjgxNA.GLFDlN.-mngSZCX9719PNDGZO5iVFRx6tpmW8FQpjfrss",
 
     Guild: "1052115524273836176",
 
-    Channel: "1403083119464157224",
+    Channel: "1343168968235552814",
 
     // Enable/disable fitur
 
